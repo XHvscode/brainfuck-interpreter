029a9e3b44c1536d2f8c012f0deea5ca84e2d8b7
lamborghini1993 <1323242382@qq.com>
2019-11-25 20:10:08 +0800

    add input

--- src/extension.ts
+++ src/extension.ts
@@ -8,27 +8,30 @@ import * as runner from './coderunner';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // Use the console to outputChannel diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "xh" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.runXH', (path) => {
+    let disposable = vscode.commands.registerCommand('extension.runXH', async (path) => {
         // The code you place here will be executed every time your command is executed
 
-        const output = vscode.window.createOutputChannel("xh Interpreter");
-        output.show();
-        try {
-            // output.appendLine(path);
-            // const sourceCode = fs.readFileSync(path.path).toString();
-            const sourceCode = "++++++ [ > ++++++++++ < - ] > +++++ .";
-            runner.runCode(sourceCode, output);
-        }
-        catch (e) {
-            output.appendLine("Error:" + e.messge);
-        }
+        await vscode.window.showInputBox({ prompt: "Input some character" }).then((input) => {
+            input = input ? input : '';
+            const outputChannel = vscode.window.createOutputChannel("xh Interpreter");
+            outputChannel.show();
+            try {
+                const sourceCode = fs.readFileSync(path.path).toString();
+                // const sourceCode = "++++++ [ > ++++++++++ < - ] > +++++ .";
+                const output = runner.runCode(sourceCode, input, outputChannel);
+            }
+            catch (e) {
+                outputChannel.appendLine("Error:" + e.messge);
+            }
+        });
+
     });
 
     context.subscriptions.push(disposable);
